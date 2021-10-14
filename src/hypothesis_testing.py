4b8ddc6d0c2ecdce97ccda199d3122614c9557af
Veronica Backer Peral <vbp@Veronicas-MacBook-Pro.local>
2021-10-13 18:09:50 -0700

    controlled hypothesis testing

--- src/hypothesis_testing.py
+++ src/hypothesis_testing.py
@@ -7,57 +7,63 @@ from respondents_treatments import question_description, results, treatment_desc
 # Hypothesis Testing
 #############################################################################
 
-control_treatment = "T8"
+def ptest(data):
 
-for question_index, question in enumerate(question_description):
-    print("="*100)
-    print(f"{question}: {question_description[question]}")
-    print("="*100)
-    print("\n")
-    for t in results:
-        if treatment_description[t] != "Control":
-            stat, pval = stats.ttest_ind(
-                results[t][question_index], results[control_treatment][question_index])
+    control_treatment = "T8"
 
-            if pval < .1:
+    for question_index, question in enumerate(question_description):
+        print("="*100)
+        print(f"{question}: {question_description[question]}")
+        print("="*100)
+        print("\n")
+        for t in data:
+            if treatment_description[t] != "Control" and len(data[t])>0 and len(data[control_treatment])>0:
+                stat, pval = stats.ttest_ind(
+                    data[t][question_index], data[control_treatment][question_index])
 
-                print("-"*100)
-                print("Two-sided t-test:")
-                print(
-                    f"{t}: {treatment_description[t]} -- Mean: {round(np.mean(results[t][question_index]),2)}")
-                print(
-                    f"{control_treatment}: {treatment_description[control_treatment]} -- Mean: {round(np.mean(results[control_treatment][question_index]),2)}")
-                print(f'P-Val: {round(pval,2)}')
-    print("\n\n\n")
+                if pval < .1:
+
+                    print("-"*100)
+                    print("Two-sided t-test:")
+                    print(
+                        f"{t}: {treatment_description[t]} -- Mean: {round(np.mean(data[t][question_index]),2)}")
+                    print(
+                        f"{control_treatment}: {treatment_description[control_treatment]} -- Mean: {round(np.mean(data[control_treatment][question_index]),2)}")
+                    print(f'P-Val: {round(pval,2)}')
+        print("\n\n\n")
 
 #############################################################################
 # Graphing
 #############################################################################
 
-for question_index, question in enumerate(question_description):
+def graph_bar(data, output_path):
+    for question_index, question in enumerate(question_description):
 
-    fig, ax = plt.subplots()
+        if len(data["T1"])==0:
+            continue
 
-    labels = [treatment_label for treatment_label in treatment_description]
+        fig, ax = plt.subplots()
 
-    means = []
-    for treatment in results:
-        means.append(np.mean(results[treatment][question_index]))
+        labels = [treatment_label for treatment_label in treatment_description]
 
-    error = []
-    for treatment in results:
-        error.append(np.std(results[treatment][question_index]))
+        means = []
+        for treatment in data:
+            means.append(np.mean(data[treatment][question_index]))
 
-    x_pos = np.arange(len(treatment_description))
+        error = []
+        for treatment in data:
+            error.append(np.std(data[treatment][question_index]))
 
-    ax.bar(x_pos, means, yerr=error, align='center',
-           alpha=0.5, ecolor='black', capsize=10)
-    ax.set_ylabel('Mean response')
-    ax.set_xticks(x_pos)
-    ax.set_xticklabels(labels)
-    ax.set_title(f'Responses to {question} by treatment')
-    ax.yaxis.grid(True)
+        x_pos = np.arange(len(treatment_description))
 
-    plt.tight_layout()
-    plt.savefig(os.path.join('..', 'graphics', f'{question}.png'))
-    # plt.show()
+        ax.bar(x_pos, means, yerr=error, align='center',
+               alpha=0.5, ecolor='black', capsize=10)
+        ax.set_ylabel('Mean response')
+        ax.set_xticks(x_pos)
+        ax.set_xticklabels(labels)
+        ax.set_title(f'Responses to {question} by treatment')
+        ax.yaxis.grid(True)
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_path, f'{question}.png'))
+        # plt.show()
